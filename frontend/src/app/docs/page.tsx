09235a81c83c79134b841802f799b8eed8d3a64c
Gouderg <victor.illien974@gmail.com>
2025-10-17 14:05:42 +0400

    update tutorial

--- frontend/src/app/docs/page.tsx
+++ frontend/src/app/docs/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-    return "kdl";
+    return (
+        <div className="flex items-center justify-center w-full h-full text-2xl font-black mt-32">
+            Work in progress 🚧
+        </div>
+    );
 }
